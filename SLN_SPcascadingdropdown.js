fa3159f00721f4995609e48baaff7f13614c306c
sylvain.lancien@gmail.com <sylvain.lancien@gmail.com>
2013-12-10 17:07:31 +0100

    Utility updated
    Minor changes to the utility : applied best practices.

--- SLN_SPcascadingdropdown.js
+++ SLN_SPcascadingdropdown.js
@@ -10,15 +10,15 @@
     $.fn.SLN_SPcascadingdropdown = function (options) {
 
         var defaults = {
-            relationshipList: "Cities", //The name of the list which contains the parent/child relationships.
-            relationshipParentList: "Countries", //The name of the list which contains the parent items.
-            relationshipParentListColumn: "Title", //The StaticName of the values column in the relationshipParentList. 
-            relationshipListChildColumn: "Title", //The StaticName of the child column in the relationshipList
-            relationshipListParentColumn: "Country", //The StaticName of the parent column in the relationshipList
-            childDropDown: "cities", //The id of the child DropDownList
+            relationshipList: "Cities", //Name of the list which contains the parent/child relationships.
+            relationshipParentList: "Countries", //Name of the list which contains the parent items.
+            relationshipParentListColumn: "Title", //StaticName of the values column in the relationshipParentList. 
+            relationshipListChildColumn: "Title", //StaticName of the child column in the relationshipList
+            relationshipListParentColumn: "Country", //StaticName of the parent column in the relationshipList
+            childDropDown: "cities", //Id of the child DropDownList
             autoFillParentDropDownList: true, //True : Fill the parent DropDownList with all ParentList values (set false if you want to keep old selected values)
             defaulFillChildDropDownList: false, //True: Fill the child DropDownList with all ChildList values if no value is selected for parent DropDownList.
-            promptText: "--Select item--" // The default text displayed in the dropdownlists
+            promptText: "--Select item--" //Default text displayed in the dropdownlists
         };
 
 
@@ -26,39 +26,39 @@
 
         return this.each(function () {
 
-            //Get the parent Dropdownlist ID
-            var parentdropdown = this.id;
+            //Get parent Dropdownlist ID
+            var parentDropDown = this.id;
 
-            //Get the current client context
-            context = SP.ClientContext.get_current();
+            //Get current client context
+            var context = SP.ClientContext.get_current();
 
-            //Fill the parent DropDownList with all values
+            //Fill parent DropDownList with all values
             if (options.autoFillParentDropDownList) {
-                fillDefaults(parentdropdown, options.relationshipParentList, options.relationshipParentListColumn, context, options.defaulFillChildDropDownList);
+                fillDefaults(parentDropDown, options.relationshipParentList, options.relationshipParentListColumn, context, options.defaulFillChildDropDownList);
             }
             else if (options.defaulFillChildDropDownList) {
-                //Fill the child DropDownList with all values
+                //Fill child DropDownList with all values
                 fillDefaults(options.childDropDown, options.relationshipList, options.relationshipListChildColumn, context, false);
             }
 
             $('#' + options.childDropDown).append("<option value='0' selected='true'>" + options.promptText + "</option>");
 
             $('select').change(function (e) {
-                if (this.id == parentdropdown) {
+                if (this.id === parentDropDown) {
                     var childList = context.get_web().get_lists().getByTitle(options.relationshipList);
 
-                    //Clear the child Dropdownlist
+                    //Clear child Dropdownlist
                     $('#' + options.childDropDown).empty();
                     $('#' + options.childDropDown).append("<option value='0' selected='true'>" + options.promptText + "</option>");
 
-                    if (this[this.selectedIndex].value != null && this[this.selectedIndex].value != 0) {
+                    if (this[this.selectedIndex].value !== null && this[this.selectedIndex].value !== 0) {
                         itemSelected = this[this.selectedIndex].text;
                         var camlQuery = new SP.CamlQuery();
 
                         camlQuery.set_viewXml("<View><Query><Where><Eq><FieldRef Name='" + options.relationshipListParentColumn + "'/>" + "<Value Type='Lookup'>" + itemSelected + "</Value></Eq></Where><OrderBy><FieldRef Name='" + options.relationshipListChildColumn + "' Ascending='True' /></OrderBy></Query><RowLimit>10</RowLimit></View>");
                         var childListItems = childList.getItems(camlQuery);
                         context.load(childListItems);
-                        context.executeQueryAsync(Function.createDelegate(this, function () { ReadListItemSucceeded("#" + options.childDropDown, options.relationshipListChildColumn, childListItems, false); }), ReadListItemFailed);
+                        context.executeQueryAsync(Function.createDelegate(this, function () { readListItemSucceeded("#" + options.childDropDown, options.relationshipListChildColumn, childListItems, false); }), readListItemFailed);
                     }
                     else {
                         if (options.defaulFillChildDropDownList) {
@@ -78,14 +78,14 @@
             function fillDefaults(dropdownName, listName, columnName, context, fillChildDropdown) {
                 var spList = context.get_web().get_lists().getByTitle(listName);
                 var camlQuery = SP.CamlQuery.createAllItemsQuery();
-                this.listItems3 = spList.getItems(camlQuery);
-                context.load(listItems3);
-                context.executeQueryAsync(Function.createDelegate(this, function () { ReadListItemSucceeded("#" + dropdownName, columnName, listItems3, fillChildDropdown); }), ReadListItemFailed);
+                var listItems = spList.getItems(camlQuery);
+                context.load(listItems);
+                context.executeQueryAsync(Function.createDelegate(this, function () { readListItemSucceeded("#" + dropdownName, columnName, listItems, fillChildDropdown); }), readListItemFailed);
             }
 
             
             //executeQueryAsync Succeed
-            function ReadListItemSucceeded(dropdownID, columnName, listItems, fillChildDropdown) {
+            function readListItemSucceeded(dropdownID, columnName, listItems, fillChildDropdown) {
                 var enumerator = listItems.getEnumerator();
 
                 //Clear the current DropDownList
@@ -96,11 +96,11 @@
                     $(dropdownID).append('<option value="' + listItem.get_id().toString() + '">' + listItem.get_item(columnName) + '</option>');
                 }
 
-                //Add the promptext as first option and select it
+                //Add promptext as first option and select it
                 $(dropdownID).prepend("<option value='0' selected='true'>" + options.promptText + "</option>");
-                $(dropdownID).find("option:first")[0].selected = true;
+                $(dropdownID).find("option:first").attr('selected', 'true');
 
-                //Fill the child DropDownList with all values : We needed to wait for the executeQueryAsync to finish
+                //Fill child DropDownList with all values : We needed to wait for the executeQueryAsync to finish
                 if (fillChildDropdown) {                    
                     fillDefaults(options.childDropDown, options.relationshipList, options.relationshipListChildColumn, context, false);
                 }
@@ -108,8 +108,8 @@
 
 
             //executeQueryAsync Failed
-            function ReadListItemFailed(sender, args) {
-                alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+            function readListItemFailed(sender, args) {
+                throw new Error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
             }
 
         });
